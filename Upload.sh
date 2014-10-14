4725a3e854f00a095e143d6e9db887c25f9495a6
System Administrator <root@walt-pachs-macbook.local>
2014-10-14 14:50:22 -0400

    Upload

--- Upload.sh
+++ Upload.sh
@@ -2,5 +2,5 @@ cd /Users/Walt/Documents/workspace/EnDob
 git init
 git add .
 git commit -m "Upload"
-git add origin git@github.com:WookiePaw/EnDob.git
+git remove add origin git@github.com:WookiePaw/EnDob.git
 git push origin master
