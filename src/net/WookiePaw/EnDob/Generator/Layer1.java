3199b7aca6ad2ff6d88be4bd8bf12eb7fa841dc6
Walt Pach <wxwsk8er@gmail.com>
2014-10-14 15:55:09 -0400

    Create Layer1.java

---
+++ src/net/WookiePaw/EnDob/Generator/Layer1.java
@@ -0,0 +1,21 @@
+/****************************************************************************************************************/
+/* Copyright 2014 Walt Pach, Lowell Davis, all rights reserved. Released under the EnDob Lesser Public License. */
+/****************************************************************************************************************/
+package net.WookiePaw.EnDob.Generator;
+
+import com.cubes.BlockTerrainControl;
+import com.cubes.Vector3Int;
+import net.WookiePaw.EnDob.Blocks.StoneBlock;
+
+
+public class Layer1 {
+    public static void a(BlockTerrainControl blockControl){
+        for(int x = 0; x < 500; x++){
+            for(int y = 0; y < 256; y++){
+                for(int z = 0; z < 500; z++){
+                    blockControl.setBlock(new Vector3Int(x, 0, z), StoneBlock.class);
+                }
+            }
+        }
+    }
+}
